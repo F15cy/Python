f78efa5bebe9894e1619da547ce33333956df951
F15cy <fcarballo@iesjoanramis.org>
2025-11-12 13:18:15 +0100

    Ex1python

---
+++ prova1.py
@@ -0,0 +1,7 @@
+a = float(input("Escriure el primer nombre: "))
+b = float(input("Escriure el segon nombre: "))
+d = float(input("Escriure el tercer nombre: "))
+c = a + b + d
+print( "El resultat de la suma {} + {} es {}".format(a, b, c,d))
+c = a * b * d
+print( "El resultat de la multiplicacio {} + {} es {}".format(a, b, d, c))
