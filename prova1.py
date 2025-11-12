2f6b7a98842c86196964dbcf6eba6109e8ae0edc
F15cy <fcarballo@iesjoanramis.org>
2025-11-12 13:43:01 +0100

    ex1

--- prova1.py
+++ prova1.py
@@ -5,3 +5,14 @@ c = a + b + d
 print( "El resultat de la suma {} + {} es {}".format(a, b, c,d))
 c = a * b * d
 print( "El resultat de la multiplicacio {} + {} es {}".format(a, b, d, c))
+if c<20:
+        print("La suma es major que 20, que es {}".format(c))
+else:   
+        print("La suma es menor que 20, que es {}".format(c))
+
+c = a * b * d
+print("El resultat de la multiplicacio {} * {} * {} es {}".format(a, b, d, c))
+if c>100:
+        print("La multiplicacio es major que 100, que es {}".format(c))
+else:
+        print("La multiplicacio es menor que 100, que es {}".format(c))
