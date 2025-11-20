dd40cba7a78b9b40b009a293ee8ce02b90e4f6b9
F15cy <fcarballo@iesjoanramis.org>
2025-11-20 11:38:12 +0100

    ex20

---
+++ ex20.py
@@ -0,0 +1,9 @@
+def invertir(cadena):
+    
+    return cadena[::-1]
+
+
+print(invertir("Soc del Ramis"))      
+print(invertir("Python"))             
+print(invertir("Hola mundo"))        
+print(invertir("12345"))             
