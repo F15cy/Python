0f38c957f4c683c93dbd506af7104c9904c41c3f
F15cy <fcarballo@iesjoanramis.org>
2025-11-20 11:44:20 +0100

    ex23

---
+++ ex23.py
@@ -0,0 +1,9 @@
+# Definir la función
+def crear_repetits(n, caracter):
+    return caracter * n
+
+# Probar la función con distintos ejemplos
+print(crear_repetits(5, "a"))   # "aaaaa"
+print(crear_repetits(3, "x"))   # "xxx"
+print(crear_repetits(0, "b"))   # ""
+print(crear_repetits(7, "*"))   # "*******"
