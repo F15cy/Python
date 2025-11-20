a51f4f0f7ced57eb631ecf959984c726bfd5cb90
F15cy <fcarballo@iesjoanramis.org>
2025-11-20 11:58:00 +0100

    ex30

---
+++ ex30.py
@@ -0,0 +1,8 @@
+# Pedimos al usuario un número binario
+binari = input("Introduce un número binario: ")
+
+# Convertimos el binario a entero usando int con base 2
+enter = int(binari, 2)
+
+# Mostramos el resultado
+print("El número en decimal es:", enter)
