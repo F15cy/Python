888379812aaaa28c6b5a6341db0cf2d93a4cbd95
F15cy <fcarballo@iesjoanramis.org>
2025-11-24 09:54:34 +0100

    ex32

---
+++ ex33.py
@@ -0,0 +1,7 @@
+# Definir la funció
+def nums_que_comencen_per(llista):
+    contador = 0
+    for nom in llista:
+        if nom[0].lower() == 'a':  # Comprova si la primera lletra és 'a'
+            contador += 1
+    return contador
