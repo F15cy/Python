2cb12ffb9364535d8ea293cc8e7956e0a2d6c17f
F15cy <fcarballo@iesjoanramis.org>
2025-12-02 09:23:52 +0100

    ex55

---
+++ ex55.py
@@ -0,0 +1,9 @@
+# Números parells fins 100
+print("Parells:")
+for i in range(2, 101, 2):
+    print(i, end=" ")
+
+print("\nSenars:")
+# Números senars fins 100
+for i in range(1, 101, 2):
+    print(i, end=" ")
